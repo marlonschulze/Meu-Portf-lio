91bf14610ab6ed166f4853fbfce500ff0e6e318e
marlonschulze <marlonschulze2804@gmail.com>
2025-06-14 08:26:16 -0300

    Funcional 100%

--- script.js
+++ script.js
@@ -1,32 +1,34 @@
-//navegação hamburguer
+document.addEventListener('DOMContentLoaded', () => {
+  const hamburger = document.getElementById('hamburger');
+  const navBar = document.getElementById('navBar');
 
-const hamburger = document.getElementById('hamburger');
-const navBar = document.getElementById('navBar');
+  hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('active');
+    navBar.classList.toggle('show');  
+  });
 
+  const links = document.querySelectorAll('.navbar a');
+  links.forEach(link => {
+    link.addEventListener('click', () => {
+      navBar.classList.remove('show');
+      hamburger.classList.remove('active');
+    });
+  });
 
-hamburger.addEventListener('click', () => {
-  hamburger.classList.toggle('active');
-  navBar.classList.toggle('show');  
+  // Máquina escrever
+  const text = "Marlon Schulze";
+  let i = 0;
+
+  function digitar() {
+    if(i < text.length){
+      document.getElementById('maquinaEscrever').textContent += text.charAt(i);
+      i++;
+      setTimeout(digitar, 150);
+    }
+  }
+
+  digitar();
 });
 
-const links = document.querySelectorAll('.navbar a');
-links.forEach(link => {
-  link.addEventListener('click', () => {
-    navBar.classList.remove('show');
-    hamburger.classList.remove('active');
-  })
-})
 
-//maquina escrever h1 header
-const text = "Marlon Schulze";
-let i = 0;
 
-function digitar(){
-  if(i < text.length){
-    document.getElementById('maquinaEscrever').textContent += text.charAt(i);
-    i++;
-    setTimeout(digitar, 150);
-  }
-}
-
-digitar();
